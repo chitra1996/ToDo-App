58c96f219cf2dfc46317e5d7eb84366b3cddcea4
chitra1996 <chitrangna.pallavi.singh@gmail.com>
2018-04-22 19:48:12 +0530

    todo input component added

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../classes/todo';
+
+@Injectable()
+export class TodoService {
+
+  private todos: Todo[];
+  private nextId: number;
+
+  constructor() {
+    this.todos = [];
+    this.nextId = 0;
+  }
+
+  public addTodo(text: string): void {
+    let todo = new Todo(this.nextId, text);
+    this.todos.push(todo);
+    this.nextId++;
+  }
+
+  public getTodos(): Todo[] {
+    return this.todos;
+  }
+
+  public removeTodo(id: number): void {
+    this.todos = this.todos.filter((todo) => todo.id != id);
+  }
+
+}
